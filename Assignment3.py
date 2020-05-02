ecd1bb64047a54b869db344eb34ce958c07d860f
satellitegazor <satellitegazor@users.noreply.github.com>
2020-05-02 16:45:53 -0500

    myfilter

--- Assignment3.py
+++ Assignment3.py
@@ -53,3 +53,37 @@ Ary = [2, 3, 4, 5, 6, 8 , 9]
 g = myreduce(sumTwo, Ary)
 print(g)
 
+
+
+
+#myfilter
+
+
+ages = [5, 12, 17, 18, 24, 32]
+
+def myFunc(x):
+  if x < 18:
+    return False
+  else:
+    return True
+
+def myfilter(func, inpAry):
+
+    if type(inpAry) != list:
+        print('Second Parameter shall be a List')
+        return
+    if type(func) != types.FunctionType:
+        print('First Parameter shall be a function')
+        return
+    if len(inpAry) < 1:
+        print('List shall contain atleast 1 element')
+        return
+
+    result = []
+    for elem in inpAry:
+        if func(elem) == True:
+            result.append(elem)
+    return result
+
+res = myfilter(myFunc, ages)
+print(res)
